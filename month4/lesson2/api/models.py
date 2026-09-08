fdceea8896aea552e4a0de57490f526032d64714
HODJAEV12 <khodjaevabdulloh0@gmail.com>
2026-09-08 16:22:23 +0600

    changes

--- month4/lesson2/api/models.py
+++ month4/lesson2/api/models.py
@@ -3,14 +3,14 @@ from django.db import models
 class Cars(models.Model):
     brand = models.CharField(max_length=130, verbose_name="Бренд")
     color = models.CharField(max_length=100, verbose_name="Цвет")
-    mileage = models.IntegerField(max_length=10, verbose_name="Пробег(км)")
+    mileage = models.IntegerField(verbose_name="Пробег(км)")
     transmission_type = models.CharField(max_length=80, verbose_name="Тип передачи")
     fuel_type = models.CharField(max_length=100, verbose_name="Тип топлива")
     car_type = models.CharField(max_length=120, verbose_name="Тип машины(легковая, грузовая)")
     car_description = models.TextField(blank=True, verbose_name="Описание машины", default="Без описания")
     state = models.TextField(blank=True, verbose_name="Состояние", default="Не указано")
     year = models.DateField(blank=True, null=True, verbose_name="Год выхода")
-    price = models.IntegerField(max_length=15, verbose_name="Цена(в долларах)")
+    price = models.IntegerField(verbose_name="Цена(в долларах)")
     image = models.ImageField(upload_to="car_image/", verbose_name="Изображение машины", null=True, blank=True)
 
     def __str__(self):
