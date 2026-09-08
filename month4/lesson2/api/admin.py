639c341d75b5df683a2a85c71bf0031877c91641
HODJAEV12 <khodjaevabdulloh0@gmail.com>
2026-09-08 16:29:28 +0600

    changes

--- month4/lesson2/api/admin.py
+++ month4/lesson2/api/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Cars
 
 @admin.register(Cars)
-class AdminBooks(admin.ModelAdmin):
+class AdminCars(admin.ModelAdmin):
     list_display = (
         "id", "brand", "color", "mileage", "transmission_type", "fuel_type", "car_type",
         "car_description", "state", "year", "price", "image"
