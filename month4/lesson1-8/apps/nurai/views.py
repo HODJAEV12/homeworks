e17297e1b3f5ed59a24eb3c7d6931086837ad377
HODJAEV12 <khodjaevabdulloh0@gmail.com>
2026-09-15 23:34:46 +0600

    changes

--- month4/lesson1-8/apps/nurai/views.py
+++ month4/lesson1-8/apps/nurai/views.py
@@ -4,3 +4,6 @@ from apps.nurai.models import Students
 def index(request):
     students = Students.objects.all()
     return render(request, "index.html", locals())
+
+def about(request):
+    return render(request, "about.html", locals())
