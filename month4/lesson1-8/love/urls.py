2da92b2feb37c2381419df29391e3cd39960f429
HODJAEV12 <khodjaevabdulloh0@gmail.com>
2026-09-15 23:35:46 +0600

    Update urls.py

--- month4/lesson1-8/love/urls.py
+++ month4/lesson1-8/love/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from apps.nurai.views import index
+from apps.nurai.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
